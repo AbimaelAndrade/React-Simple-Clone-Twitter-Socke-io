0cde4c90ca7be424243fdb93ac3ef4bd7ac9a82f
abimaelandrade <abimaelc.andrade@yahoo.com.br>
2018-11-08 11:30:15 -0300

    Event login, save local storage and navegate route /timeline

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -4,12 +4,40 @@ import LoginWrapper, { Form, Input, Button } from './styles';
 import { ReactComponent as LogoTwitter } from '../../assets/twitter.svg';
 
 export default class Login extends Component {
+
+  state = {
+    username: '',
+  }
+
+  handleFormSubmit = (event) => {
+    event.preventDefault()
+    const { username } = this.state
+
+    if(!username.length) return
+
+    localStorage.setItem('@CloneTwitter:username', username)
+    this.props.history.push('/timeline')
+  }
+
+  handleInputChange = (event) => {
+    console.log(event.target.value)
+    this.setState({
+      username: event.target.value
+    })
+  }
+
   render() {
+
+    const { username }= this.state
     return (
       <LoginWrapper>
         <LogoTwitter />
-        <Form>
-          <Input placeholder="Nome do usuário"/>
+        <Form onSubmit={this.handleFormSubmit}>
+          <Input 
+            value={ username }
+            onChange={this.handleInputChange } 
+            placeholder="Nome do usuário"
+          />
           <Button>Entrar</Button> 
         </Form>
       </LoginWrapper>
